f8b939234dfd60e78bef79c294c04d184b1550da
sohanur-rahman0 <sajeebsrs@gmail.com>
2022-09-13 03:08:50 +0600

    fixed cors issue on video delete

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # How to run the application
 ```
 npm install
-rename the .env.example file to .env [provide the mongodb connection string]
+rename the .env.example file to .env [provide the mongodb connection string & app must run at port 3000]
 npm run dev
 ```
 
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const logger = require('morgan')
 const session = require('express-session')
 const flash = require('connect-flash')
 const eta = require('eta')
+const cors = require('cors')
 const router = require('./routes/index')
 
 const app = express()
@@ -20,6 +21,7 @@ app.use(
     saveUninitialized: true,
   })
 )
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
